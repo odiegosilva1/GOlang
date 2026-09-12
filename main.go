0784af18674113a6f55ab4dd2a327314c2085dc6
Diego Silva <odiegosilvadev@gmail.com>
2026-09-12 18:29:59 -0300

    update main.go

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// API
+	http.HandleFunc("/task", handleTasks)
+	http.HandleFunc("/task/", handleTaskByID)
+}
+
+func handleTasks(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "tasks")
+}
+
+func handleTaskByID(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "task by id: %s", r.URL.Path)
+
+	// Frontend
+	fs := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fs)
+
+	fmt.Println("Servidor rodando em http://localhost:8000")
+	http.ListenAndServe(":8000", nil)
 }
